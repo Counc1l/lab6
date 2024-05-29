d4a3a860d41c1ead4b3b04bf32dd67bf5e0ba693
Grant <bassleg@wwu.edu>
2024-05-29 11:34:12 -0700

    Updating

--- app/src/test/java/lab6/ComponentsTest.java
+++ app/src/test/java/lab6/ComponentsTest.java
@@ -6,9 +6,9 @@ package lab6;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class ComponentTest {
+public class ComponentsTest {
     @Test public void appHasAGreeting() {
-        App classUnderTest = new Component();
+        App classUnderTest = new Components();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
